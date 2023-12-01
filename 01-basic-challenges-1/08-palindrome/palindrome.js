c641b3ae8117cf5c2b3c97b02a06d94c33f6d067
nnsh93 <nicholasngshihhao@gmail.com>
2023-12-01 16:59:42 +0800

    basic-challenge-1: palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,7 @@
-function isPalindrome() {}
+function isPalindrome(value) {
+  const trimValue = value.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  return trimValue.split("").reverse().join("") === trimValue;
+}
 
 module.exports = isPalindrome;
